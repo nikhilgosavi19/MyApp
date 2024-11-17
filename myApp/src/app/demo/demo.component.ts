ff05f9f825bd3f92f9a20d8326b754f4ac5d67e9
nikhilgosavi19 <gosavi.nikhil19@gmail.com>
2024-11-17 20:15:49 +0530

    assignment no 4

---
+++ myApp/src/app/demo/demo.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  template: `
+    <h2>Demo Component</h2>
+    <p>This is Marvellous Infosystems</p>
+  `,
+  styles: [`
+    h2 {
+      color: blue;
+    }
+  `]
+})
+export class DemoComponent { 
+
+}
